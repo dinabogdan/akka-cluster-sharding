d8c069ec3bef72099529c8c7c4d769707e6cf787
dinabogdan <bogdan.dina03@gmail.com>
2020-06-05 14:02:52 +0300

    message protocol and routes

--- src/main/scala/com/reactivebbq/orders/OrderRoutes.scala
+++ src/main/scala/com/reactivebbq/orders/OrderRoutes.scala
@@ -3,10 +3,12 @@ package com.reactivebbq.orders
 import java.util.UUID
 
 import akka.actor.ActorRef
+import akka.pattern.ask
 import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{ExceptionHandler, Route}
 import akka.util.Timeout
+import com.reactivebbq.orders.OrderActor.ItemAddedToOrder
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
@@ -17,8 +19,11 @@ class OrderRoutes(orderActors: ActorRef)(implicit ec: ExecutionContext)
   private implicit val timeout: Timeout = Timeout(5.seconds)
 
   private def exceptionHandler: ExceptionHandler = ExceptionHandler {
+    case ex: OrderActor.OrderNotFoundException =>
+      complete(HttpResponse(StatusCodes.NotFound, entity = ex.getMessage))
     case ex =>
       complete(HttpResponse(StatusCodes.InternalServerError, entity = ex.getMessage))
+
   }
 
   lazy val routes: Route =
@@ -27,29 +32,33 @@ class OrderRoutes(orderActors: ActorRef)(implicit ec: ExecutionContext)
         post {
           entity(as[OrderActor.OpenOrder]) { openOrder =>
             complete {
-              ???
+              val orderId = OrderId()
+              (orderActors ? OrderActor.Envelope(orderId, openOrder))
+                .mapTo[OrderActor.OrderOpened]
+                .map(_.order)
             }
           }
         } ~
-        pathPrefix(Segment) { id =>
-
-          val orderId = OrderId(UUID.fromString(id))
-
-          get {
-            complete {
-              ???
-            }
-          } ~
-          path("items") {
-            post {
-              entity(as[OrderActor.AddItemToOrder]) { addItemToOrder =>
-                complete {
-                  ???
+          pathPrefix(Segment) { id =>
+            val orderId = OrderId(UUID.fromString(id))
+            get {
+              complete {
+                (orderActors ? OrderActor.Envelope(orderId, OrderActor.GetOrder()))
+                  .mapTo[Order]
+              }
+            } ~
+              path("items") {
+                post {
+                  entity(as[OrderActor.AddItemToOrder]) { addItemToOrder =>
+                    complete {
+                      (orderActors ? OrderActor.Envelope(orderId, addItemToOrder))
+                        .mapTo[ItemAddedToOrder]
+                        .map(_.order)
+                    }
+                  }
                 }
               }
-            }
           }
-        }
       }
     }
 }
